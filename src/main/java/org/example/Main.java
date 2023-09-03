55cb5c1a7c1d70c3445c997672eec5d27380e0d5
ssuni00 <run9986@naver.com>
2023-09-03 15:13:22 +0900

    Changes

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Student s1 = new Student("2190000", "홍길동");
+        System.out.println(s1.toString());
+    }
+}
